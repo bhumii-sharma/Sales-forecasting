63491fade8359447df357c0003693e2c3b3cd783
bhumii-sharma <bhumi6117@gmail.com>
2024-11-27 14:41:34 +0530

    changes in files

--- training/configuration_manager/configuration.py
+++ training/configuration_manager/configuration.py
@@ -1,6 +1,5 @@
 import os
 from training.constants import *  # Ensure constants are well-defined
-from training.configuration_manager.configuration import ConfigManager
 from training.entity.config_entity import (
     DataIngestionConfig,
     DataValidationConfig,
