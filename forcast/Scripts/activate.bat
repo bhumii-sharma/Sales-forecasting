a53c1e5699d49c45c17cb83d952762c0c4018633
Pushpendra52005 <pushpendramaurya143654@gmail.com>
2024-11-30 15:47:53 +0530

    remove the tree.py fle code

--- forcast/Scripts/activate.bat
+++ forcast/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Sale_forcasting\Sales-forecasting\forcast
+set VIRTUAL_ENV=D:\SALE\Sales-forecasting\forcast
 
 if not defined PROMPT set PROMPT=$P$G
 
